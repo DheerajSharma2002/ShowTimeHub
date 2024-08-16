801c69124ed511dcdbebb9644af160f42a175d5e
dheeraj <ds817897@gmail.com>
2024-08-16 20:00:09 +0530

    master: first commit

---
+++ ShowTimeHub/src/main/java/com/ravi/ShowTimeHub/ShowTimeHubApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.ShowTimeHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowTimeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowTimeHubApplication.class, args);
+	}
+
+}
